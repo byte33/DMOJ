76799867aa6a3445d0cfb71afe1468ce9e2645d8
Matteo T <mltullo1016@gmail.com>
2019-02-05 18:02:11 -0500

    Ci V0.2
    Added input from file as option.

--- DMOJ/Ci.py
+++ DMOJ/Ci.py
@@ -1,19 +1,32 @@
 print("Hello and welcome to byte33's cipher program")
-print("02/05/19 V0.1")
+print("02/05/19 V0.2\n")
+
 
 upperList = list()
 lowerList = list()
 newMessageList = list()
 newMessage = ""
+global message
 
-message = input("Enter your message:\t")
-message = message.split()
+typ = int(input("How would you like to enter the message?\n1 for manual entry\n2 for file entry\n"))
+if typ == 1:
+    message = input("Enter your message:\t")
+    message = message.split()
+
+elif typ == 2:
+    path = input("Please copy and paste the path to the file into the console.")
+    rFile = open(path, "r")
+    message = rFile.readline()
+    rFile.close()
+    message = message.split()
+
+else:
+    print("Not a valid input")
 
 
 #The amount to shift
 shiftNum = int(input("How much shift?\t"))
 shiftDir = input("In what direction? (L/R)\t")
-
 
 #Make list of uppercase characters
 for upper in range(26):
